92766fecb1826710acb38327e45b40dd8ceec9d3
godnoob3103 <135569620+godnoob3103@users.noreply.github.com>
2024-06-12 22:07:00 +0700

    Update faster text bomb.py

--- make text bomb.py
+++ make text bomb.py
@@ -1,18 +1,29 @@
 import random
 import string
-import time
+import threading
 
 def generate_random_text():
-    text = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(100, 1000)))
-    return text
+    return ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(100, 1000)))
 
 def save_text_to_file(text):
     with open("text.txt", "a") as file:  # Open file in append mode
         file.write(text + "\n")
 
-if __name__ == "__main__":
+def worker():
     while True:
         random_text = generate_random_text()
         save_text_to_file(random_text)
-        print("Random text appended to text.txt")
-        time.sleep(0.1)  # Adding a short delay to prevent system overload
+
+if __name__ == "__main__":
+    num_threads = 10  # Adjust the number of threads based on your system's capabilities
+    threads = []
+    
+    # Create and start the worker threads
+    for _ in range(num_threads):
+        thread = threading.Thread(target=worker)
+        thread.start()
+        threads.append(thread)
+    
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
